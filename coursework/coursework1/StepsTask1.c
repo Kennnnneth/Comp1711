4091710b08619cdf49fe0990e5e18994120c81e2
Kenneth David Umudjro <146677743+Kennnnneth@users.noreply.github.com>
2023-11-02 16:19:56 +0000

    Coursework1 Modified

--- coursework/coursework1/StepsTask1.c
+++ coursework/coursework1/StepsTask1.c
@@ -11,8 +11,6 @@ typedef struct {
 
 // Define any additional variables here
 
-
-
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
@@ -43,6 +41,28 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
+    FITNESS_DATA data[1000] = {};
+    int i = 0, buffer_size = 100;
+    char temp[100], line_buffer[buffer_size], filename[] = "FitnessData_2023.csv";
 
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
 
+    while (fgets(line_buffer, buffer_size, file) != NULL) {
+        tokeniseRecord(line_buffer, ",", data[i].date, data[i].time, temp);
+        data[i].steps = atoi(temp);
+        i += 1;
+    }
+
+    printf("Number of records in file: %d\n", i);
+
+    for (i = 0; i < 3; i++) {
+        printf("%s/%s/%d\n", data[i].date, data[i].time, data[i].steps);
+    }
+
+    fclose(file);
+    return 0;
 }
